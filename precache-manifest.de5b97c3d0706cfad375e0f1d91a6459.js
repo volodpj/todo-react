65490cd862611b8d30b163987a1996e3692d2320
volodpj <volod.af@gmail.com>
2019-06-10 15:33:34 +0300

    Updates

---
+++ precache-manifest.de5b97c3d0706cfad375e0f1d91a6459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d40284aacec586299f0f11adcb1f0b5",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "cf2e881a05146d7d51ba",
+    "url": "/todo-react/static/css/main.d45fafe0.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/todo-react/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "cf2e881a05146d7d51ba",
+    "url": "/todo-react/static/js/main.a9d7a70a.chunk.js"
+  },
+  {
+    "revision": "cd203911fc85a7f73b39",
+    "url": "/todo-react/static/js/runtime~main.6292bb99.js"
+  }
+]);
